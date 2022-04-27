6e7bf7f99891105a02e2ad3b7042eea2f9be516a
andalons <andreacor001@gmail.com>
2022-04-27 11:12:19 +0200

    add profes repository yuhu

--- src/components/App.js
+++ src/components/App.js
@@ -12,56 +12,73 @@ function App() {
       console.log(data[0]);
     });
   }, []);
-  const [numberOfErrors, setNumberOfErrors] = useState(0);
-  const [lastLetter, setLastLetter] = useState('');
-  const [message, setMessage] = useState('');
+
+  //constantes de estado
   const [word, setWord] = useState('');
   const [userLetters, setUserLetters] = useState([]);
-  const renderSolutionLetters = () => {
-    const wordLetters = word.split('');
-    console.log(wordLetters);
-    return wordLetters.map((letter, index) => {
-      if (userLetters.includes(letter)) {
-        console.log(wordLetters);
-        return (
-          <li className="letter" key={index}>
-            {letter}
-          </li>
-        );
-      } else {
-        return <li className="letter" key={index}></li>;
-      }
-    });
+  const [lastLetter, setLastLetter] = useState('');
+
+  // events
+
+  const handleKeyDown = (ev) => {
+    // Sabrías decir para qué es esta línea
+    ev.target.setSelectionRange(0, 1);
   };
-  const renderErrorLetters = () => {
-    return userLetters.map((letter, index) => {
-      if (!word.includes(letter)) {
-        return (
-          <li className="letter" key={index}>
-            {letter}
-          </li>
-        );
-      }
-    });
-  };
-  const handleInput = (ev) => {
-    const newValue = ev.target.value;
-    setLastLetter(newValue);
-    if (newValue.match('[A-Za-zÑñÁÉÍÓÚáéíóú]') !== null) {
-      console.log(newValue);
-      const lettersUsed = [...userLetters, newValue];
-      setUserLetters(lettersUsed);
-      console.log('Es valida');
-      setMessage('');
-    } else {
-      console.log('Introduce otro valor');
-      setMessage('Introduce otro valor');
+
+  const handleChange = (ev) => {
+    let re = /[a-zA-Z]/; //add regular pattern - lesson 3.3 exercise 2
+    if (re.test(ev.target.value)) {
+      handleLastLetter(ev.target.value);
     }
   };
-  const handlePaint = () => {
-    const changeNumber = numberOfErrors + 1;
-    setNumberOfErrors(changeNumber);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
   };
+
+  const getNumberOfErrors = () => {
+    const errorLetters = userLetters.filter(
+      (letter) => word.includes(letter) === false
+    );
+    return errorLetters.length;
+  };
+
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map((letter, index) => {
+      const exists = userLetters.includes(letter.toLocaleLowerCase());
+      return (
+        <li key={index} className="letter">
+          {exists ? letter : ''}
+        </li>
+      );
+    });
+  };
+
+  const renderErrorLetters = () => {
+    const errorLetters = userLetters.filter(
+      (letter) =>
+        word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
+    );
+    return errorLetters.map((letter, index) => {
+      return (
+        <li key={index} className="letter">
+          {letter}
+        </li>
+      );
+    });
+  };
+
+  const handleLastLetter = (value) => {
+    value = value.toLocaleLowerCase();
+    setLastLetter(value);
+
+    if (!userLetters.includes(value)) {
+      userLetters.push(value);
+      setUserLetters([...userLetters]);
+    }
+  };
+
   return (
     <div className="page">
       <header>
@@ -69,9 +86,6 @@ function App() {
       </header>
       <main className="main">
         <section>
-          <button type="button" onClick={handlePaint}>
-            Incrementar
-          </button>
           <div className="solution">
             <h2 className="title">Solución:</h2>
             <ul className="letters">{renderSolutionLetters()}</ul>
@@ -80,11 +94,12 @@ function App() {
             <h2 className="title">Letras falladas:</h2>
             <ul className="letters">{renderErrorLetters()}</ul>
           </div>
-          <form className="form">
+          <form className="form" onSubmit={handleSubmit}>
             <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
+              autoFocus
               autoComplete="off"
               className="form__input"
               maxLength="1"
@@ -92,25 +107,25 @@ function App() {
               name="last-letter"
               id="last-letter"
               value={lastLetter}
-              onChange={handleInput}
+              onKeyDown={handleKeyDown}
+              onChange={handleChange}
             />
-            <p className="text">{message}</p>
           </form>
         </section>
-        <section className={`dummy error-${numberOfErrors}`}>
+        <section className={`dummy error-${getNumberOfErrors()}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
           <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+          <span className="error-9  line"></span>
+          <span className="error-8  line"></span>
+          <span className="error-7  line"></span>
+          <span className="error-6  head"></span>
+          <span className="error-5  line"></span>
+          <span className="error-4  line"></span>
+          <span className="error-3  line"></span>
+          <span className="error-2  line"></span>
+          <span className="error-1  line"></span>
         </section>
       </main>
     </div>
