ce6822e4777bb95712cd1fd22222cc06d30b4e79
andalons <andreacor001@gmail.com>
2022-04-27 10:52:16 +0200

    merge codes

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,7 @@ import '../styles/App.scss';
 import blackboard from '../images/blackboard.jpg';
 import { useState, useEffect } from 'react';
 import getWord from '../services/callToApi';
-
-// Cread la funcionalidad necesaria (un servicio por aquí, un useEffect por allá...) para que al arrancar la página se haga una petición a https://palabras-aleatorias-public-api.herokuapp.com/random y se guarde el resultado en la constante de estado word.
-// Si al refrescar la página veis que en DevTools > Componentes hay una palabra aleatoria es que lo habéis hecho bien. Además, los guiones de la solución deben tener la misma longitud que la palabra.
+import Header from './Header';
 
 function App() {
   //FETCH
@@ -14,50 +12,56 @@ function App() {
       console.log(data[0]);
     });
   }, []);
-
-  //BOTÓN INCREMENTAR
   const [numberOfErrors, setNumberOfErrors] = useState(0);
-  const handleBtnUp = () => {
-    setNumberOfErrors(numberOfErrors + 1);
-    console.log(numberOfErrors + 1);
-  };
-
-  //FUNCIÓN COMPROBAR ELTRA VÁLIDA
-  const checkValueInput = (valueInput) => {
-    if (valueInput.match('[a-zA-ZñÑáéíóúÁÉÍÓÚ]') !== null) {
-      setLastLetter(valueInput);
-      return true;
-    } else {
-      setLastLetter('');
-      console.log('la letra no es válida');
-      return false;
-    }
-  };
-
-  //GUARDAR ÚLTIMA LETRA
   const [lastLetter, setLastLetter] = useState('');
-  const handleInput = (ev) => {
-    const valueInput = ev.target.value;
-    const checkedInput = checkValueInput(valueInput);
-    if (valueInput !== '' && checkedInput) {
-      // checkedInput === true (es lo mismo)
-      setUserLetters([...userLetters, valueInput]);
-    }
-    console.log(userLetters);
-  };
-
-  //PINTAR GUIONES
+  const [message, setMessage] = useState('');
   const [word, setWord] = useState('');
+  const [userLetters, setUserLetters] = useState([]);
   const renderSolutionLetters = () => {
     const wordLetters = word.split('');
+    console.log(wordLetters);
     return wordLetters.map((letter, index) => {
-      return <li key={index} className="letter"></li>;
+      if (userLetters.includes(letter)) {
+        console.log(wordLetters);
+        return (
+          <li className="letter" key={index}>
+            {letter}
+          </li>
+        );
+      } else {
+        return <li className="letter" key={index}></li>;
+      }
     });
   };
-
-  //CREAR ARRAY PARA LETRAS INTRODUCIDAS
-  const [userLetters, setUserLetters] = useState([]);
-
+  const renderErrorLetters = () => {
+    return userLetters.map((letter, index) => {
+      if (!word.includes(letter)) {
+        return (
+          <li className="letter" key={index}>
+            {letter}
+          </li>
+        );
+      }
+    });
+  };
+  const handleInput = (ev) => {
+    const newValue = ev.target.value;
+    setLastLetter(newValue);
+    if (newValue.match('[A-Za-zÑñÁÉÍÓÚáéíóú]') !== null) {
+      console.log(newValue);
+      const lettersUsed = [...userLetters, newValue];
+      setUserLetters(lettersUsed);
+      console.log('Es valida');
+      setMessage('');
+    } else {
+      console.log('Introduce otro valor');
+      setMessage('Introduce otro valor');
+    }
+  };
+  const handlePaint = () => {
+    const changeNumber = numberOfErrors + 1;
+    setNumberOfErrors(changeNumber);
+  };
   return (
     <div className="page">
       <header>
@@ -65,32 +69,16 @@ function App() {
       </header>
       <main className="main">
         <section>
+          <button type="button" onClick={handlePaint}>
+            Incrementar
+          </button>
           <div className="solution">
             <h2 className="title">Solución:</h2>
-
-            <ul className="letters">
-              {renderSolutionLetters()}
-              {/*    <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li> */}
-            </ul>
+            <ul className="letters">{renderSolutionLetters()}</ul>
           </div>
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
+            <ul className="letters">{renderErrorLetters()}</ul>
           </div>
           <form className="form">
             <label className="title" htmlFor="last-letter">
@@ -103,8 +91,10 @@ function App() {
               type="text"
               name="last-letter"
               id="last-letter"
+              value={lastLetter}
               onChange={handleInput}
             />
+            <p className="text">{message}</p>
           </form>
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
@@ -122,10 +112,7 @@ function App() {
           <span className="error-2 line"></span>
           <span className="error-1 line"></span>
         </section>
-      </main>{' '}
-      <button type="button" onClick={handleBtnUp}>
-        Incrementar
-      </button>
+      </main>
     </div>
   );
 }
